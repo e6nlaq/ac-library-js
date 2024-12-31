5898df25f239f93c36e086b69c62d54c1eea6c11
e6nlaq <120777733+e6nlaq@users.noreply.github.com>
2024-12-31 12:23:42 +0900

    add test

--- test/utils/limit.ts
+++ test/utils/limit.ts
@@ -1,4 +1,5 @@
 export const min_ll = -(1n << 63n);
 export const max_ll = (1n << 63n) - 1n;
 export const max_uint = 2 ** 32 - 1;
+export const min_int = -(2 ** 31);
 export const max_int = 2 ** 31 - 1;
