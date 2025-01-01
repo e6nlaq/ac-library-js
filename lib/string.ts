3ce5e3060052a30ac71b60f6f77e8a2091fd1cad
e6nlaq <120777733+e6nlaq@users.noreply.github.com>
2025-01-01 12:13:53 +0900

    `internal.sa_is`

--- lib/string.ts
+++ lib/string.ts
@@ -41,4 +41,134 @@ export namespace internal {
 
 		return sa;
 	}
+
+	export function sa_is(
+		s: Int32Array,
+		upper: number,
+		THRESHOLD_NAIVE = 10,
+		THRESHOLD_DOUBLING = 40
+	): Int32Array {
+		const n = s.length;
+		if (n === 0) return new Int32Array();
+		if (n === 1) return new Int32Array([0]);
+		if (n === 2) {
+			if (s[0] < s[1]) {
+				return new Int32Array([0, 1]);
+			}
+			return new Int32Array([1, 0]);
+		}
+		if (n < THRESHOLD_NAIVE) {
+			return sa_naive(s);
+		}
+		if (n < THRESHOLD_DOUBLING) {
+			return sa_doubling(s);
+		}
+
+		const sa = new Int32Array(n);
+		const ls = new Array<boolean>(n).fill(false);
+		for (let i = n - 2; i >= 0; i--) {
+			ls[i] = s[i] === s[i + 1] ? ls[i + 1] : s[i] < s[i + 1];
+		}
+		const sum_l = new Int32Array(upper + 1);
+		const sum_s = new Int32Array(upper + 1);
+		for (let i = 0; i < n; i++) {
+			if (!ls[i]) {
+				sum_s[s[i]]++;
+			} else {
+				sum_l[s[i] + 1]++;
+			}
+		}
+		for (let i = 0; i <= upper; i++) {
+			sum_s[i] += sum_l[i];
+			if (i < upper) sum_l[i + 1] += sum_s[i];
+		}
+
+		const induce = (lms: Int32Array) => {
+			sa.fill(-1);
+			const buf = new Int32Array(upper + 1);
+			sum_s.set(buf);
+			for (const d of lms) {
+				if (d === n) continue;
+				sa[buf[s[d]]++] = d;
+			}
+			sum_l.set(buf);
+			sa[buf[s[n - 1]]++] = n - 1;
+			for (let i = 0; i < n; i++) {
+				const v = sa[i];
+				if (v >= 1 && !ls[v - 1]) {
+					sa[buf[s[v - 1]]++] = v - 1;
+				}
+			}
+			sum_l.set(buf);
+			for (let i = n - 1; i >= 0; i--) {
+				const v = sa[i];
+				if (v >= 1 && ls[v - 1]) {
+					sa[--buf[s[v - 1] + 1]] = v - 1;
+				}
+			}
+		};
+
+		// std::vector<int> lms_map(n + 1, -1);
+		const lms_map = new Int32Array(n + 1).fill(-1);
+		let m = 0;
+		for (let i = 1; i < n; i++) {
+			if (!ls[i - 1] && ls[i]) {
+				lms_map[i] = m++;
+			}
+		}
+
+		const lms = new Array<number>();
+		for (let i = 1; i < n; i++) {
+			if (!ls[i - 1] && ls[i]) {
+				lms.push(i);
+			}
+		}
+
+		induce(new Int32Array(lms));
+
+		if (m) {
+			const sorted_lms = new Array<number>();
+			for (const v of sa) {
+				if (lms_map[v] !== -1) sorted_lms.push(v);
+			}
+			const rec_s = new Int32Array(m);
+			let rec_upper = 0;
+			rec_s[lms_map[sorted_lms[0]]] = 0;
+			for (let i = 1; i < m; i++) {
+				let l = sorted_lms[i - 1];
+				let r = sorted_lms[i];
+				const end_l = lms_map[l] + 1 < m ? lms[lms_map[l] + 1] : n;
+				const end_r = lms_map[r] + 1 < m ? lms[lms_map[r] + 1] : n;
+				let same = true;
+				if (end_l - l !== end_r - r) {
+					same = false;
+				} else {
+					while (l < end_l) {
+						if (s[l] !== s[r]) {
+							break;
+						}
+						l++;
+						r++;
+					}
+					if (l === n || s[l] !== s[r]) same = false;
+				}
+				if (!same) rec_upper++;
+				rec_s[lms_map[sorted_lms[i]]] = rec_upper;
+			}
+
+			const rec_sa = sa_is(
+				rec_s,
+				rec_upper,
+				THRESHOLD_NAIVE,
+				THRESHOLD_DOUBLING
+			);
+
+			for (let i = 0; i < m; i++) {
+				sorted_lms[i] = lms[rec_sa[i]];
+			}
+			// induce(sorted_lms);
+			induce(new Int32Array(sorted_lms));
+		}
+		return sa;
+	}
 } // namespace internal
