f13bcebfa9fcb11048e8c4c503277e1090294bce
sakamotor <120777733+e6nlaq@users.noreply.github.com>
2025-01-02 19:55:55 +0900

    Create scc.ts

---
+++ example/scc.ts
@@ -0,0 +1,24 @@
+// Verified: https://atcoder.jp/contests/practice2/submissions/61317841
+
+import { SCCGraph } from 'ac-library-js/scc';
+import { readFileSync } from 'node:fs';
+
+function main(inp: string[][]): void {
+	const [N, M] = inp[0].map(Number);
+	const G = new SCCGraph(N);
+
+	for (let i = 1; i <= M; i++) {
+		const [a, b] = inp[i].map(Number);
+		G.add_edge(a, b);
+	}
+
+	const ans = G.scc();
+	console.log(ans.length);
+	for (let i = 0; i < ans.length; i++) console.log(`${ans[i].length} ${ans[i].join(' ')}`);
+}
+
+main(
+	readFileSync('/dev/stdin', 'utf-8')
+		.split('\n')
+		.map(line => line.split(' ')),
+);
